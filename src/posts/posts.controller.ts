6efe09a94051be562bb329ed13c70da0eb332fce
zhaoxuan <xuanzhao@88.com>
2021-10-22 00:05:33 +0800

    add: 添加增删改查

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
+
+class CreatePostDto {
+  @ApiProperty({ description: '帖子标题' })
+  title: string;
+  @ApiProperty({ description: '帖子内容' })
+  content: string;
+}
 
 @Controller('posts')
 @ApiTags('帖子')
@@ -11,17 +18,36 @@ export class PostsController {
   }
 
   @Post()
-  create() {
-    return {
-      success: true,
-    };
+  @ApiOperation({ summary: '创建帖子' })
+  create(@Body() body: CreatePostDto) {
+    return body;
   }
 
   @Get(':id')
-  detail() {
+  @ApiOperation({ summary: '帖子详情' })
+  detail(@Param('id') id: string) {
     return {
-      id: 1,
+      id: id,
       title: '111',
+    };
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: '编辑帖子' })
+  update(@Param('id') id: string, @Body() body: CreatePostDto) {
+    return {
+      success: true,
+      id,
+      body,
+    };
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除帖子' })
+  remove(@Param('id') id: string) {
+    return {
+      id,
+      sucucess: true,
     };
   }
 }
